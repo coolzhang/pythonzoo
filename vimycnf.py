571845276f2bdfe452acffe5879687055ecf3ada
coolzhang <https://github.com/coolzhang>
2013-09-21 18:49:00 +0800

    update vimycnf.py

--- vimycnf.py
+++ vimycnf.py
@@ -9,8 +9,10 @@ import ConfigParser
 
 USAGE = """%prog - Show and modify option from the configuration file of mysqld
 e.g.:
-%prog --mycnf /path/to/my.cnf --section mysqld --show variable
-%prog --mycnf /path/to/my.cnf --section mysqld --set variable=value
+#show variable:>   %prog -f /path/to/my.cnf -g mysqld -s variable
+#set  variable:>   %prog -f /path/to/my.cnf -g mysqld -m variable=value
+#add  variable:>   %prog -f /path/to/my.cnf -g mysqld -a variable=value
+#list sections:>   %prog -f /path/to/my.cnf -l
 """
 VERSION = "%prog Alpha"
 
@@ -25,6 +27,10 @@ optparser.add_option("-s", "--show", action='store', dest="getopt",
                      help="Get an option value for the named section.")
 optparser.add_option("-m", "--set", action='store', dest="setopt",
                      help="Set the given option to the specified value.")
+optparser.add_option("-l", "--list", action='store_true', dest="group",
+                     help="List all sections in the configuration.")
+optparser.add_option("-a", "--append", action='store', dest="addopt",
+                     help="Append new option into the existing section.")
 
 (options, args) = optparser.parse_args()
 
@@ -34,13 +40,11 @@ config.read(options.mycnf)
 def show_variable():
     try:
         if options.getopt == 'all':
-            print "The whole variables are as follow:"
-            print "----------------------------------"
+            print "[%s]" %options.section
             for name, value in config.items(options.section):
                 print "%-35s = %s" %(name, value)
         else:
-            print "type(optins.getopt) options.getopt"
-            print "current variable: [ %s = %s ]" \
+            print "Current variable:  %s = %s" \
                   %(options.getopt, config.get(options.section, options.getopt))
 
     except ConfigParser.NoSectionError, err:
@@ -50,28 +54,65 @@ def show_variable():
 
 def set_variable():
     try:
-        if args:
-            options.setopt = options.setopt + ''.join(args)
-
-        (option, value) = tuple(options.setopt.replace(' ','').split('='))
-        print "before modifying: [ %s = %s ]" \
-              %(option, config.get(options.section, option))
-        config.set(options.section, option, value)
-        with open(options.mycnf, 'w') as mycnf:
-            config.write(mycnf)
-        print "after modifying:  [ %s = %s ]" \
-              %(option, config.get(options.section, option))
+        options.setopt = options.setopt + ''.join(args)
+        if "=" not in options.setopt:
+            print "Argument not available: should be variable=value"
+        else:
+            (option, value) = tuple(options.setopt.replace(' ','').split('='))
+            if value:
+                print "Before modifying: %s = %s" \
+                      %(option, config.get(options.section, option))
+                config.set(options.section, option, value)
+                with open(options.mycnf, 'w') as mycnf:
+                     config.write(mycnf)
+                print "After modifying:  %s = %s" \
+                      %(option, config.get(options.section, option))
+            else:
+                print "Argument not available: should be variable=value"
     except ConfigParser.NoSectionError, err:
         print str(err)
     except ConfigParser.NoOptionError, err:
         print str(err)
 
+def add_variable():
+    options.addopt = options.addopt + ''.join(args)
+    if "=" not in options.addopt:
+        print "Argument not available: should be variable=value"
+    else:
+        (option, value) = tuple(options.addopt.replace(' ','').split('='))
+        if value:
+            if config.has_option(options.section, option):
+                print "Variable: '%s' already exists, should use --set|-m option" \
+                      %option
+            if config.has_section(options.section) and not config.has_option(options.section, option):
+                config.set(options.section, option, value)
+                with open(options.mycnf, 'w') as mycnf:
+                    config.write(mycnf)
+                print "Append new variable: %s = %s" \
+                      %(option, config.get(options.section, option))
+            else:
+                print "No section: '%s'" %options.section
+        else:
+            print "Argument not available: should be variable=value"
+
+def list_section():
+    print "%d sections in %s :" %(len(config.sections()), options.mycnf)
+    for section in config.sections():
+        print "[%s]" %section
+
+
 if __name__ == "__main__":
     if options.getopt and options.setopt:
-        print optparser.error("option -s and -m are mutually exclusive.")
+        print optparser.error("Option -s and -m are mutually exclusive.")
 
     if options.getopt:
         show_variable()
 
     if options.setopt:
         set_variable()
+
+    if options.group:
+        list_section()
+
+    if options.addopt:
+        add_variable()
